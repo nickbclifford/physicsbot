27c1e23ff55d584fa6a7f213f0fb9d875410d2cb
Nick Clifford <nick@nickclifford.me>
2018-11-20 17:13:31 -0600

    add manual HoF command

--- src/HoF.ts
+++ src/HoF.ts
@@ -1,18 +1,38 @@
-import { MessageReaction, RichEmbed, TextChannel, User } from 'discord.js';
+import { Message, MessageReaction, RichEmbed, TextChannel, User } from 'discord.js';
 import config from './config';
 
-export default async (messageReaction: MessageReaction) => {
-	const msg = messageReaction.message;
-
-	const channel = msg.guild.channels.find(c => c.name === config.hallOfFame.channel) as TextChannel;
+export async function addToHallOfFame(message: Message) {
+	const channel = message.guild.channels.find(c => c.name === config.hallOfFame.channel) as TextChannel;
 	if (!channel) {
 		return;
 	}
 
-	const emoji = msg.guild.emojis.find(e => e.name === config.hallOfFame.emoji);
+	const emoji = message.guild.emojis.find(e => e.name === config.hallOfFame.emoji);
 	if (!emoji) {
 		return;
 	}
+
+	await message.react(emoji);
+
+	const embed = new RichEmbed()
+		.setColor(message.member.displayColor)
+		.setFooter('Hall of Fame')
+		.setTimestamp()
+		.addField('User', message.member.nickname || message.author.username, true)
+		.addField('Channel', (message.channel as TextChannel).name, true);
+
+	if (message.content.length !== 0) {
+		embed.addField('Message', message.content);
+	}
+	if (message.attachments.size !== 0) {
+		embed.setImage(message.attachments.array()[0].url);
+	}
+
+	await channel.send(embed);
+}
+
+export default async (messageReaction: MessageReaction) => {
+	const msg = messageReaction.message;
 
 	if (config.hallOfFame.reactionLimit === 0) {
 		return;
@@ -21,23 +41,12 @@ export default async (messageReaction: MessageReaction) => {
 		return;
 	}
 
+	const emoji = msg.guild.emojis.find(e => e.name === config.hallOfFame.emoji);
+	if (!emoji) {
+		return;
+	}
+
 	if (messageReaction.emoji.id === emoji.id && messageReaction.count >= config.hallOfFame.reactionLimit) {
-		await msg.react(emoji);
-
-		const embed = new RichEmbed()
-			.setColor(msg.member.displayColor)
-			.setFooter('Hall of Fame')
-			.setTimestamp()
-			.addField('User', msg.member.nickname || msg.author.username, true)
-			.addField('Channel', (msg.channel as TextChannel).name, true);
-
-		if (msg.content.length !== 0) {
-			embed.addField('Message', msg.content);
-		}
-		if (msg.attachments.size !== 0) {
-			embed.setImage(msg.attachments.array()[0].url);
-		}
-
-		await channel.send(embed);
+		await addToHallOfFame(msg);
 	}
 };
---
+++ src/commands/hof.ts
@@ -0,0 +1,39 @@
+import { Message, TextChannel } from 'discord.js';
+import config from '../config';
+import { addToHallOfFame } from '../HoF';
+import { Command } from '../types';
+
+export default {
+	async run(client, message, args) {
+		if (message.author.id !== config.botOwnerID) {
+			await message.channel.send('You do not have permission to run this command.');
+			return;
+		}
+
+		if (!args[0]) {
+			await message.channel.send('Missing message ID.');
+			return;
+		}
+
+		let channel = message.channel as TextChannel;
+		if (args[1]) {
+			channel = message.guild.channels.get(args[1])! as TextChannel;
+		}
+
+		let hofMessage: Message;
+		try {
+			hofMessage = await channel.fetchMessage(args[0]);
+		} catch (e) {
+			await message.channel.send(`Error finding message. (\`${e.message}\`)`);
+			return;
+		}
+
+		await addToHallOfFame(hofMessage);
+		await message.channel.send('Message successfully added to Hall of Fame.');
+	},
+	help: {
+		usage: 'hof <message id> <channel id [optional]>',
+		description: 'Manually adds a message to the Hall of Fame. ' +
+			'(Defaults to current channel if no ID provided, only usable by bot owner.)'
+	}
+} as Command;
